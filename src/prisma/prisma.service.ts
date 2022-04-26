37f70f9528db235d81122ff2fad6358a484b89c6
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-04-26 15:53:54 +0700

    adding jwt features

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:9e1tK*VnoAGN@localhost:5432/portofolio_app?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+    console.log({ config });
   }
 }
