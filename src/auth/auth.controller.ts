80d23d2362b9888498d93fe55100d310ad51dece
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-04-26 15:58:26 +0700

    fixing misdirect status code on sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -9,6 +9,8 @@ export class AuthController {
   signUp(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
+
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
